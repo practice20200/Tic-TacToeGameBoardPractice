8beb72c920db26a317180a6463d80a4d47e184cd
practice20200 <wor9hard2018@gmail.com>
2022-02-11 20:11:29 -0800

    2nd Commit Designed the layout of circles

--- TicTacToeBoardgame/ContentView.swift
+++ TicTacToeBoardgame/ContentView.swift
@@ -8,9 +8,37 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let colums: [GridItem] = [
+        GridItem(.flexible()),
+        GridItem(.flexible()),
+        GridItem(.flexible())
+    ]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        VStack{
+            
+            Spacer()
+            
+            LazyVGrid(columns: colums){
+                ForEach(0..<9) { i in
+                    ZStack{
+                        Circle()
+                            .foregroundColor(.blue).opacity(0.5)
+                            .frame(width: 80, height: 80)
+                        Image(systemName: "circle")
+                            .resizable()
+                            .frame(width: 40, height: 40, alignment: .center)
+                            .foregroundColor(.white)
+                    }
+                }
+            }
+            
+            Spacer()
+            
+        }
+
     }
 }
 
