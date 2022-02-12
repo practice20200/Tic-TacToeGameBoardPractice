4862e4dda789bb7615d621d09026732cb9b4ee0f
practice20200 <wor9hard2018@gmail.com>
2022-02-11 19:43:59 -0800

    Initial Commit

---
+++ TicTacToeBoardgame/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TicTacToeBoardgame
+//
+//  Created by Apple New on 2022-02-11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TicTacToeBoardgame/TicTacToeBoardgameApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeBoardgameApp.swift
+//  TicTacToeBoardgame
+//
+//  Created by Apple New on 2022-02-11.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeBoardgameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
